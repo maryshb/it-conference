4884eb240e0ca8b84e0b4c4a90a9e57aa53f5695
maryshb <mariajewula28@gmail.com>
2021-12-20 00:42:15 +0100

    conf: model mapper
    Signed-off-by: maryshb <mariajewula28@gmail.com>

--- src/main/java/sii/itconference/ItConferenceApplication.java
+++ src/main/java/sii/itconference/ItConferenceApplication.java
@@ -1,13 +1,21 @@
 package sii.itconference;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ItConferenceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ItConferenceApplication.class, args);
+    }
+
+    //TODO przenieść do konfiguracji
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }

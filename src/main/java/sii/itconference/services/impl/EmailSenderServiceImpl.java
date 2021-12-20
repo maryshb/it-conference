fce7bd5e2047e358f410c4cae15c95829e5813b3
maryshb <mariajewula28@gmail.com>
2021-12-20 18:53:20 +0100

    feat: add service to handle notifications
    Signed-off-by: maryshb <mariajewula28@gmail.com>

---
+++ src/main/java/sii/itconference/services/impl/EmailSenderServiceImpl.java
@@ -0,0 +1,52 @@
+package sii.itconference.services.impl;
+
+import org.springframework.stereotype.Service;
+import sii.itconference.model.Reservation;
+import sii.itconference.model.User;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class EmailSenderServiceImpl {
+
+    Writer writer = null;
+
+    public void sendMail(User user, Reservation reservation) {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("notification_" + reservation.getReservationId() + ".txt"), StandardCharsets.UTF_8))) {
+            writer.write(generateMail(user, reservation));
+        } catch (IOException e) {
+            System.out.println("Couldn't write to file");
+        } finally {
+            try {
+                writer.close();
+            } catch (Exception ex) {
+                System.out.println("Couldn't close the writer");
+            }
+        }
+    }
+
+    private String generateMail(User user, Reservation reservation) {
+
+        return getFormattedDate() + "\nZapisałeś się na zajęcia: "
+                + getLectureName(reservation) + "\nDo zobaczenia " + getReceiver(user);
+    }
+
+    private String getFormattedDate() {
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+        return formatter.format(date);
+    }
+
+    private String getReceiver(User user) {
+        return user.getUsername();
+    }
+
+    private String getLectureName(Reservation reservation) {
+        return reservation.getLecture().getName();
+    }
+}
